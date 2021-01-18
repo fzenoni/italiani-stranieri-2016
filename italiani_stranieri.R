cc1f518af280eab5ccfa51eee362d6aebb934cd0
fzenoni <florianzenoni@gmail.com>
2021-01-18 16:30:52 +0100

    New colors and better legend

--- italiani_stranieri.R
+++ italiani_stranieri.R
@@ -6,9 +6,9 @@ library(RColorBrewer)
 
 
 # Build the data
-data <- data.frame(name = c("Italiani", "Stranieri con cittadinanza italiana",
-                            "Immigrati regolari", "Immigrati clandestini",
-                            "In attesa di documenti o rimpatrio", "Rifugiati"),
+data <- data.frame(name = c("Italiani (88.4 %)", "Stranieri con cittadinanza italiana (2.1 %)",
+                            "Immigrati regolari (8.2 %)", "Immigrati clandestini (0.8 %)",
+                            "In attesa di documenti o rimpatrio (0.3 %)", "Rifugiati (0.2 %)"),
                    pc = c(88.4, 2.1, 8.2, 0.8, 0.3, 0.2))
 
 # Waffle chart
@@ -26,7 +26,9 @@ rightOrd <- sort(table(var), decreasing=TRUE) %>% names
 
 # Define colors
 colors <- plasma(6)
-customPalette <- c(colors[4], colors[2], colors[5], colors[1], colors[6], colors[3])
+# customPalette <- c(colors[4], colors[2], colors[5], colors[1], colors[6], colors[3])
+colors <- c(grey.colors(n=5, start = 0.1, end = 0.9), '#5794b2')
+colors <- c(colors[1], colors[2], colors[3], colors[6], colors[4], colors[5])
 
 # Plot
 ggplot(df, aes(x = x, y = y, fill = category)) + 
@@ -35,7 +37,7 @@ ggplot(df, aes(x = x, y = y, fill = category)) +
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0), trans = 'reverse') +
   # scale_fill_brewer(breaks = rightOrd, palette = 'Paired') +
-  scale_fill_manual(breaks = rightOrd, values = customPalette) +
+  scale_fill_manual(breaks = rightOrd, values = colors) +
   labs(title = 'Italiani e stranieri in Italia (2016)',
        caption = 'Fonti: ilpost.it, ismu.org, ilsole24ore.com, lenius.it, unhcr.it\nrielaborato da un post di @maurovanetti') +
   theme(panel.border = element_rect(color = "white", size = 1, fill = NA),
@@ -44,4 +46,4 @@ ggplot(df, aes(x = x, y = y, fill = category)) +
         axis.title = element_blank(),
         axis.ticks = element_blank(),
         legend.title = element_blank(),
-        legend.position = "right")
+        legend.position = "bottom")
